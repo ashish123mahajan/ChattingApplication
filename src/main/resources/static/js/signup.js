d255e2ea5ab78fe12a3d578b42208e5d64541d43
Ashish <ash.n.mahajan@gmail.com>
2019-01-27 16:21:19 +0530

    Signup flow added

---
+++ src/main/resources/static/js/signup.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$("#signup").click(function(){
+		var user={};
+		user.email=$('#email').val();
+		user.password=$('#password').val();
+		user.lname=$('#lname').val();
+		user.fname=$('#fname').val();
+		user.phone=$('#phone').val();
+		$.ajax({
+			  type: "POST",
+			  url: baseurl+"/signup",
+			  contentType:"application/json; charset=utf-8",
+			  data: JSON.stringify(user),
+			  dataType : 'json',
+			  success: function(result){
+				
+			        window.location=baseurl+"/login.html";
+			  },
+			  error: function(XMLHttpRequest, textStatus, errorThrown) {
+			     alert("please enter valid credentials");
+				  window.location=baseurl+"signup.html";
+			  }
+			});
+	    
+	  });
+	
+	//console.log(getCookieValue("globalUserId"));
+	});
